217f0bfdac450ebfb29d27002898c64105db0d4e
separation <yw.amy.lin@gmail.com>
2019-01-28 18:48:36 +0800

    ex3: cost function with regularization

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,17 +36,12 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h = sigmoid(X * theta);
+J = 1 / m * (-y' * log(h) - (1 - y)' * log(1 - h)) + lambda / 2 / m * (theta(2:end)' * theta(2:end));
 
-
-
-
-
-
-
-
+grad = 1 / m * (X' * (h - y));
+grad(2:end) += lambda / m * theta(2:end);
 
 % =============================================================
-
-grad = grad(:);
 
 end
