59812612d6c3492ecb55148ed7bab0b1c1fe74af
separation <yw.amy.lin@gmail.com>
2019-01-28 21:12:45 +0800

    ex3: predict from pre-trained NN parameters

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,13 +21,9 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
-
-
-
-
+a1 = sigmoid([ones(m, 1) X] * Theta1');
+a2 = sigmoid([ones(size(a1, 1), 1) a1] * Theta2');
+[dummy, p] = max(a2, [], 2);
 
 % =========================================================================
 
